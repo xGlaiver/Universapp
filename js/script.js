6bac326ca7556c904d94778b5882f3fa20e7d669
xGlaiver <63293370+xGlaiver@users.noreply.github.com>
2020-12-05 18:37:46 +0100

    Contants sistemato, body overflow nascosto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.querySelector(".open-links").addEventListener("click",function(){
     document.querySelector(".overlay").style.height = "100%";
+    document.querySelector("body").style.overflow = "hidden";
   });
   
   document.querySelector(".close-links").addEventListener("click",function(){
     document.querySelector(".overlay").style.height = 0;
+    document.querySelector("body").style.overflow = "visible";
   });
